7cd960b70c38517f39f855ea4f2b104bcbb7fbd5
Choper03 <csutucdeleon@gmail.com>
2023-09-22 00:10:36 -0600

    first commit

---
+++ ParcialDesarrolloWeb/Contact.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="ParcialDesarrolloWeb.Contact" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div style="padding:15px">
+
+        <table class="w-70" style="height: 287px; width: 1208px">
+            <tr>
+                <td colspan="2" style="font-size: xx-large; background-color: #333333; color: #FFFFFF; height: 46px;">Ventas&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="text-end" style="height: 29px; width: 604px">&nbsp;</td>
+                <td class="text-start" style="height: 29px; text-align: justify; width: 604px">&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="text-end" style="height: 30px; width: 604px">
+                    <asp:Label ID="Label1" runat="server" Font-Size="Medium" Text="Codigo:"></asp:Label>
+                </td>
+                <td class="text-start" style="height: 30px; text-align: justify; width: 604px">
+                    <asp:TextBox ID="TxtCodigo" runat="server" Font-Size="Medium" TextMode="Number" Width="200px"></asp:TextBox>
+                &nbsp;
+                    <asp:Button ID="BtnBuscar" runat="server" BackColor="#666666" Font-Bold="True" Font-Size="Large" ForeColor="White" Text="Buscar" OnClick="BtnBuscar_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td class="text-end" style="width: 604px; height: 30px">
+                    <asp:Label ID="Label2" runat="server" Font-Size="Medium" Text="Seleccione un Cliente:"></asp:Label>
+                </td>
+                <td class="text-start" style="height: 30px; width: 604px">
+                    <asp:DropDownList ID="dropDownListClientes" runat="server" AutoPostBack="True">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            
+            <tr>
+                <td class="text-end" style="width: 604px; height: 30px">
+                    <asp:Label ID="Label4" runat="server" Font-Size="Medium" Text="Serie de Factura:"></asp:Label>
+                </td>
+                <td class="text-start" style="height: 30px; width: 604px">
+                    <asp:TextBox ID="TxtSerie" runat="server" Font-Size="Medium" Width="700px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="text-end" style="width: 604px; height: 30px">
+                    <asp:Label ID="Label5" runat="server" Font-Size="Medium" Text="Numero de Factura:"></asp:Label>
+                </td>
+                <td class="text-start" style="height: 30px; width: 604px">
+                    <asp:TextBox ID="TxtNumero" runat="server" Font-Size="Medium" Width="500px" ></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="text-end" style="width: 604px; height: 30px">
+                    <asp:Label ID="Label6" runat="server" Font-Size="Medium" Text="Monto:"></asp:Label>
+                </td>
+                <td class="text-start" style="height: 30px; width: 604px">
+                    <asp:TextBox ID="TxtMonto" runat="server" Font-Size="Medium" Width="500px" ></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="text-end" style="width: 604px; height: 30px">
+                    <asp:Label ID="Label7" runat="server" Font-Size="Medium" Text="Mensaje:"></asp:Label>
+                </td>
+                <td class="text-start" style="height: 30px; width: 604px">
+                    <asp:TextBox ID="TxtMensaje" runat="server" Font-Size="Medium" Width="500px" Enabled="False"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 604px; height: 30px">&nbsp;</td>
+                <td style="height: 30px; width: 604px">&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 604px; height: 30px">&nbsp;</td>
+                <td class="text-start" style="height: 30px; width: 604px">
+                    <asp:Button ID="BtnGuardar" runat="server" BackColor="#666666" Font-Bold="True" Font-Size="Large" ForeColor="White" Text="Guardar" OnClick="BtnGuardar_Click" />
+                    <asp:Button ID="BtnActualizar" runat="server" BackColor="#666666" Font-Bold="True" Font-Size="Large" ForeColor="White" Text="Pagar" OnClick="BtnActualizar_Click" />
+                </td>
+            </tr>
+        </table>
+        <asp:GridView ID="VistaVenta" runat="server" BackColor="#666666" Font-Bold="True" ForeColor="White" Width="1208px">
+            <Columns>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="chk" runat="server" AutoPostBack="True" OnCheckedChanged="chk_CheckedChanged" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+
+</asp:Content>
+
---
+++ ParcialDesarrolloWeb/Contact.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ParcialDesarrolloWeb
+{
+    public partial class Contact : Page
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext("");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var clientes = from cliente in db.CLIENTES
+                               select new
+                               {
+                                   ID = cliente.CODIGO,
+                                   Nombre = cliente.NOMBRE
+                               };
+
+
+                dropDownListClientes.DataSource = clientes;
+                dropDownListClientes.DataTextField = "Nombre"; // Nombre del campo a mostrar
+                dropDownListClientes.DataValueField = "ID";
+                dropDownListClientes.DataBind();
+            }
+        }
+
+        
+
+        string codigoClienteSeleccionado = "";
+
+        protected void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int codigo = int.Parse(TxtCodigo.Text);
+                int codigoCliente = int.Parse(dropDownListClientes.SelectedValue);
+                string fecha = DateTime.Now.ToString("yyyy-MM-dd");
+                string serie = TxtSerie.Text;
+                decimal numero = decimal.Parse(TxtNumero.Text);
+                decimal monto = decimal.Parse(TxtMonto.Text);
+                var saldo = (from cliente in db.CLIENTES where cliente.CODIGO == codigoCliente select cliente).First();
+                decimal presupuesto = (decimal)saldo.SALDO;
+                if(presupuesto >= monto) { 
+                var st = new VENTAS
+                    {
+                        CODIGO = codigo,
+                        CODIGO_CLIENTE = codigoCliente,
+                        FECHA = fecha,
+                        SERIE_FACTURA = serie,
+                        NUMERO_FACTURA = numero,
+                        MONTO = monto,
+                        ESTADO = "ACTIVO"
+
+                    };
+                    db.VENTAS.InsertOnSubmit(st);
+                    db.SubmitChanges();
+
+                var clientesaldo = (from cliente in db.CLIENTES where cliente.CODIGO == codigoCliente select cliente).First();
+                clientesaldo.SALDO = clientesaldo.SALDO - monto;
+                db.SubmitChanges();
+
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", "alert('Ingreso de datos');", true);
+                    this.TxtMensaje.Text = "";
+                    cargaDatos();
+                    limpiarCarjas();
+                }else
+                {
+                    this.TxtMensaje.Text = "Saldo Insuficiente, Realice sus pagos lo mas pronto posible";
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", "alert('" + ex.Message.ToString() + "');", true);
+            }
+
+        }
+       
+
+
+        protected void BtnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int codigo = int.Parse(TxtCodigo.Text);
+                string codigoClienteSeleccionado = dropDownListClientes.SelectedValue;
+                int codigoCliente = int.Parse(codigoClienteSeleccionado.ToString());
+                decimal monto = decimal.Parse(TxtMonto.Text);
+
+                var stupdate = (from cliente in db.CLIENTES where cliente.CODIGO == codigoCliente select cliente).First();
+                decimal saldo = decimal.Parse(stupdate.SALDO.ToString());
+                stupdate.SALDO = saldo + monto;
+                db.SubmitChanges();
+
+                var stupdate1 = (from venta in db.VENTAS where venta.CODIGO == codigo select venta).First();
+                stupdate1.ESTADO = "INACTIVO";
+                db.SubmitChanges();
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", "alert('Actualizado los datos');", true);
+                TxtMonto.Enabled = true;
+                cargaDatos();
+                limpiarCarjas();
+            }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", "alert('" + ex.Message.ToString() + "');", true);
+            }
+
+        }
+
+        void cargaDatos()
+        {
+            var query = from venta in db.VENTAS where venta.ESTADO == "ACTIVO" select venta;
+            VistaVenta.DataSource = query;
+            VistaVenta.DataBind();
+        }
+
+        void limpiarCarjas()
+        {
+            this.TxtCodigo.Text = "";
+            this.TxtSerie.Text = "";
+            this.TxtNumero.Text = "";
+            this.TxtMonto.Text = "";
+            TxtCodigo.Focus();
+        }
+
+        protected void chk_CheckedChanged(object sender, EventArgs e)
+        {
+            int rowind = ((GridViewRow)(sender as Control).NamingContainer).RowIndex;
+            CheckBox cd = (CheckBox)VistaVenta.Rows[rowind].FindControl("chk");
+            if (cd.Checked == true)
+            {
+                TxtCodigo.Text = VistaVenta.Rows[rowind].Cells[1].Text;
+                dropDownListClientes.SelectedValue = VistaVenta.Rows[rowind].Cells[2].Text;
+                TxtMonto.Text = VistaVenta.Rows[rowind].Cells[6].Text;
+                TxtMonto.Enabled = false;
+            }
+            else
+            {
+                this.TxtCodigo.Text = "";
+                this.TxtMonto.Text = "";
+                TxtCodigo.Focus();
+            }
+        }
+
+
+        protected void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            if (TxtCodigo.Text != string.Empty)
+            {
+                var stBuscar = from venta in db.VENTAS where venta.CODIGO == int.Parse(TxtCodigo.Text) && venta.ESTADO == "ACTIVO" select venta;
+                VistaVenta.DataSource = stBuscar;
+                VistaVenta.DataBind();
+            }
+        }
+         void cargarClientes()
+        {
+
+                var clientes = from cliente in db.CLIENTES select cliente;
+                dropDownListClientes.DataSource = clientes;
+                dropDownListClientes.DataTextField = "Nombre"; 
+                dropDownListClientes.DataValueField = "Codigo"; 
+                dropDownListClientes.DataBind();
+            
+        }
+    }
+}
